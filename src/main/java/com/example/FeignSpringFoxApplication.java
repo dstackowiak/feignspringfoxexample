7553b4bbedf783bcf5a69e60ee0a75f474be5542
Mike Boddin <Mike.Boddin@ecg-leipzig.de>
2015-12-03 09:42:53 +0100

    workaround for NPE in Springfox

--- src/main/java/com/example/FeignSpringFoxApplication.java
+++ src/main/java/com/example/FeignSpringFoxApplication.java
@@ -1,17 +1,25 @@
 package com.example;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableSwagger2
-public class FeignSpringFoxApplication {
+public class FeignSpringFoxApplication implements ApplicationContextAware{
 
     public static void main(String[] args) {
         SpringApplication.run(FeignSpringFoxApplication.class, args);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext ac) throws BeansException {
+        //force the bean to get loaded as soon as possible
+        ac.getBean("requestMappingHandlerAdapter");
     }
 }
